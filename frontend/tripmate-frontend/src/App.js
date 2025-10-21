031bab2e4090c8ebaa9068a9f8d8d1a00b164ed5
OJK <xenoficx69420@outlook.sg>
2025-10-21 23:36:38 +0800

    Add files via upload

---
+++ frontend/tripmate-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TripPlanner from "./components/TripPlanner";
+
+function App() {
+  return (
+    <div className="App">
+      <TripPlanner />
+    </div>
+  );
+}
+
+export default App;
